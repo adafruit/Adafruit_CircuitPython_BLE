82c00ac209e653017e7776b6f9f73f293a5d459f
Dan Halbert <halbert@halwitz.org>
2019-02-01 11:02:37 -0500

    Improve UARTServer example

--- examples/ble_uart_echo_test.py
+++ examples/ble_uart_echo_test.py
@@ -1,15 +1,19 @@
 from adafruit_ble.uart import UARTServer
 
 uart = UARTServer()
-uart.start_advertising()
 
-# Wait for a connection
-while not uart.connected:
-    pass
+while True:
+    uart.start_advertising()
 
-# When the client disconnects, the program will exit.
-while uart.connected:
-    # Returns b'' if nothing was read.
-    one_byte = uart.read(1)
-    if one_byte:
-        uart.write(one_byte)
+    # Wait for a connection
+    while not uart.connected:
+        pass
+
+    while uart.connected:
+        # Returns b'' if nothing was read.
+        one_byte = uart.read(1)
+        if one_byte:
+            uart.write(one_byte)
+
+    # When disconnected, arrive here. Go back to the top
+    # and start advertising again.
