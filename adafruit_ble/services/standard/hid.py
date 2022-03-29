de7cacfbb4bddf74f5188f23b56f075c884f98c4
Alec Delaney <tekktrik@gmail.com>
2022-03-29 18:18:04 -0400

    "Reformatted per new black version"

--- adafruit_ble/services/standard/hid.py
+++ adafruit_ble/services/standard/hid.py
@@ -390,7 +390,7 @@ class HIDService(Service):
             i += size
 
         def get_report_info(collection, reports):
-            """ Gets info about hid reports """
+            """Gets info about hid reports"""
             for main in collection["mains"]:
                 if "type" in main:
                     get_report_info(main, reports)
