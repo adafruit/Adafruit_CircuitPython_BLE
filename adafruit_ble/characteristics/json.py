cb33c86f43a937d5552764d269f364c301ea6144
mraleson <markraleson@outlook.com>
2022-04-16 13:27:35 -0600

    Add JSON characteristic

---
+++ adafruit_ble/characteristics/json.py
@@ -0,0 +1,53 @@
+"""
+`json`
+====================================================
+
+This module provides Json characteristic.
+
+"""
+
+import json
+from . import Attribute
+from . import Characteristic
+
+__version__ = "0.0.0-auto.0"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_BLE.git"
+
+
+class JsonCharacteristic(Characteristic):
+    """Json string characteristic."""
+
+    def __init__(
+        self,
+        *,
+        uuid=None,
+        properties=Characteristic.READ,
+        read_perm=Attribute.OPEN,
+        write_perm=Attribute.OPEN,
+        initial_value=None,
+    ):
+        super().__init__(
+            uuid=uuid,
+            properties=properties,
+            read_perm=read_perm,
+            write_perm=write_perm,
+            max_length=512,
+            fixed_length=False,
+            initial_value=self.pack(initial_value),
+        )
+
+    @staticmethod
+    def pack(value):
+        return json.dumps(value).encode("utf-8")
+
+    @staticmethod
+    def unpack(value):
+        return json.loads(str(value, "utf-8"))
+
+    def __get__(self, obj, cls=None):
+        if obj is None:
+            return self
+        return self.unpack(super().__get__(obj, cls))
+
+    def __set__(self, obj, value):
+        super().__set__(obj, self.pack(value))
