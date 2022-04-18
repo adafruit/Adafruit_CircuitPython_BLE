5eb2b5b4d6c3c5232b593c9c0a2ff25d91f1c546
mraleson <markraleson@outlook.com>
2022-04-18 17:59:31 -0600

    Add Json Characteristic examples

---
+++ examples/ble_json_service.py
@@ -0,0 +1,30 @@
+from adafruit_ble.uuid import VendorUUID
+from adafruit_ble.services import Service
+from adafruit_ble.characteristics import Characteristic
+from adafruit_ble.characteristics.json import JsonCharacteristic
+
+
+# A custom service with two Json characteristics for this device.  The "sensors" characteristic
+# provides updated sensor values for any connected device to read.  The "settings" characteristic
+# can be changed by any connected device to update the peripheral's settings.  The UUID of your
+# service can be any valid random uuid (some BLE UUID's are reserved).
+# NOTE: Json data is limited by characteristic max_length of 512 byes.
+class SensorService(Service):
+    uuid = VendorUUID("51ad213f-e568-4e35-84e4-67af89c79ef0")
+
+    settings = JsonCharacteristic(
+        uuid=VendorUUID("e077bdec-f18b-4944-9e9e-8b3a815162b4"),
+        properties=Characteristic.READ | Characteristic.WRITE,
+        initial_value={
+            'unit': 'celsius'
+        }
+    )
+
+    sensors = JsonCharacteristic(
+        uuid=VendorUUID("528ff74b-fdb8-444c-9c64-3dd5da4135ae"),
+        properties=Characteristic.READ,
+    )
+
+    def __init__(self, service=None):
+        super().__init__(service=service)
+        self.connectable = True
